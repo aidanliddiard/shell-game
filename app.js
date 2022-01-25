a8b9ed5ef57478d6a9baa272539a7ee87ce8a284
aidanliddiard <aidanliddiard@gmail.com>
2022-01-24 18:03:35 -0800

    app.js function for button one and added pearl in home.css

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 // import functions and grab DOM elements
+const shellOneBtn = document.getElementById('shell-1-btn');
+const shellTwoBtn = document.getElementById('shell-2-btn');
+const shellThreeBtn = document.getElementById('shell-3-btn');
+
+const pearl1 = document.getElementById('pearl-1');
+const pearl2 = document.getElementById('pearl-2');
+const pearl3 = document.getElementById('pearl-3');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('totalGames');
 
 // let state
 
+let correctGuess = 0;
+let totalGuesses = 0;
+
 // set event listeners 
+shellOneBtn.addEventListener('click', ()=> {
+    pearl1.classList.remove('reveal');
+    pearl2.classList.remove('reveal');
+    pearl3.classList.remove('reveal');
+
+    totalGuesses++;
+
+    const pearlLocation = Math.ceil(Math.random() *3);
+
+    if (pearlLocation === 1) {
+        pearl1.classList.add('reveal');
+        correctGuess++;
+    }
+    else if (pearlLocation === 2) {
+        pearl2.classList.add('reveal');
+    }
+    else {
+        pearl3.classList.add('reveal');
+    }
+});
+
+winsEl.textContent = correctGuess;
+totalEl.textContent = totalGuesses;
+lossesEl.textContent = totalGuesses - correctGuess;
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
